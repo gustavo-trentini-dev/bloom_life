40956ab8add6e0730a723850798d56b07b0b760a
Gustavo <gustavo.trentini@universo.univates.br>
2019-12-05 21:28:51 -0300

    Ajustes rota web service

--- lib/graph.dart
+++ lib/graph.dart
@@ -182,11 +182,11 @@ class _GraphState extends State<Graph> {
 
   String _localhost() {
     if (Platform.isAndroid)
-//      return 'http://177.44.248.24:3000/';
-      return 'http://10.0.2.2:3000/';
+      return 'http://177.44.248.24:3000/';
+//      return 'http://10.0.2.2:3000/';
     else // for iOS simulator
-//      return 'http://177.44.248.24:3000/';
-      return 'http://localhost:3000/';
+      return 'http://177.44.248.24:3000/';
+//      return 'http://localhost:3000/';
   }
 }
 
